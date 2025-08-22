54e5c46643fede5fb8df018096c0f94384810b82
LAPTOP-46U7O433\szksr <szksrt@gmail.com>
2025-08-22 19:45:47 +0900

    production.pyの修正

--- wam/settings/production.py
+++ wam/settings/production.py
@@ -1,15 +1,22 @@
 from .base import *
 import os
-
-from dotenv import load_dotenv
-load_dotenv()
-
+import dj_database_url
 
 DEBUG = False
 
-SECRET_KEY = os.environ['SECRET_KEY']  # 必須
+SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = ['ota-juku-new.onrender.com']
+DATABASE_URL = os.environ['DATABASE_URL']
+
+ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+DATABASES = {
+    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=600, ssl_require=True)
+}
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
